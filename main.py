1dd2dec23145193f3adb4ae2d3a840fa2c4fca08
aikr-2008 <2026A.Ikromkhono@arkpioneer.org>
2024-09-10 19:53:06 +0000

    Made some changes to the mini game. Introduced a verification process.

--- main.py
+++ main.py
@@ -9,10 +9,19 @@ print("Probably ice cream, what about you?")
 print("Ngl, probably a steak. Any hobbies?")
 print("Not really, I do like playing sodoku")
 
-input("Welcome to a simple game. Pick a number from 1 to 10: ")
-if input == "1":
+#Mini game.
+number = input("Welcome to a simple game. Pick a number from 1 to 10: ")
+if number == "1":
   print("You have won a coin!")
-elif input >"1":
+elif number >"1":
   print("You have won a book!")   
-elif input > "6":
+elif number > "6":
   print("You have won a pen!")
+
+#Verification process.
+email = input("Enter your email:  ")
+password = input("Enter your password:  ")
+if password == "arkpioneer":
+  print("Welcome to the admin hub!")
+else:
+  print("Error. Wrong password.") 
