5ce6613a4c08f84d00deeb704af498c5f112942e
aikr-2008 <2026A.Ikromkhono@arkpioneer.org>
2024-09-10 19:35:45 +0000

    Trying a new "if" statement for a mini game.

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ print("What's your favourite food?")
 print("Probably ice cream, what about you?")
 print("Ngl, probably a steak. Any hobbies?")
 print("Not really, I do like playing sodoku")
+
+input("Welcome to a simple game. Pick a number from 1 to 10: ")
+if input == "1":
+  print("You have won a coin!")
+elif input >"1":
+  print("You have won a book!")   
+elif input > "6":
+  print("You have won a pen!")
